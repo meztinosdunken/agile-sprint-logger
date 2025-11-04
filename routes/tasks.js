64ca00cdd14f81434992e18244da2c8fc02885e7
meztinosdunken <orlanmorry5462@hotmail.com>
2025-11-04 05:13:01 -0600

    feat: initial commit for routes/tasks.js

---
+++ routes/tasks.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const Task = require('../models/Task');
+const router = express.Router();
+
+// Create a new task
+router.post('/', async (req, res) => {
+  const task = new Task(req.body);
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Get all tasks
+router.get('/', async (req, res) => {
+  const tasks = await Task.find();
+  res.send(tasks);
+});
+
+module.exports = router;
